7caa43e20371272fbef475154e912b3f10859639
Maher Hawash <gmhawash@gmail.com>
2013-07-02 05:20:50 -0700

    Perform the cascade on the function from the paper.

--- spec/algorithm/cascade_spec.rb
+++ spec/algorithm/cascade_spec.rb
@@ -43,19 +43,81 @@ describe Algorithm::Cascade do
       TXT
 
       subject = described_class.new inputs, outputs
-      subject.render.inspect.should == strip_leading(<<-TXT)
-        c c - - - - - -
-        . c - - - - - -
-        . + c - - - - -
-        . . c - - - - -
-        . . + c - - c -
-        . . . n - - c -
-        . . . + c c - -
+      # NOTE: This is a temporary solution and it does not represent the
+      # actual layout; the intersection points above the c|n should really
+      # have the don't care symbol '-'.
+      subject.render.inspect.gsub('-','.').should == strip_leading(<<-TXT)
+        c c . . . . . .
+        . c . . . . . .
+        . + c . . . . .
+        . . c . . . . .
+        . . + c . . c .
+        . . . n . . c .
+        . . . + c c . .
         . . . . . . + c
         . . . . n c . c
         . . . . + . . +
         . . . . . + . .
         + . . . . . . .
+      TXT
+    end
+
+    it 'converts activation table into gates' do
+      inputs = text_grid <<-TXT
+        c . . . . . . . . .
+        . c - - - - - - - -
+        . . . . . . . . . .
+        . n - - - - - - - -
+        . n - - - - - - - -
+        . n - - - - - - - c
+        . n - - - - - - - c
+        . n - - - c - - - n
+        . n - - - c - - - n
+        . n - c - n - c - n
+        . n c n c n c n c -
+      TXT
+      outputs = text_grid <<-TXT
+        1 0 1 0 0 0 1 0 0 1
+        0 1 0 0 1 1 0 0 1 0
+        0 0 0 1 0 0 0 1 0 0
+      TXT
+
+      subject = described_class.new inputs, outputs
+      # NOTE: This is a temporary solution and it does not represent the
+      # actual layout; the intersection points above the c|n should really
+      # have the don't care symbol '-'.
+      subject.render.inspect.gsub('-','.').should == strip_leading(<<-TXT)
+        c c . . . . . . . . . . . . . . . . . . . . . . . . .
+        . c . . . . . . . . . . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . . . . . . . . . . . . .
+        . + c . . . . . . . . . . . . . . . . . . . . . . . .
+        . . n . . . . . . . . . . . . . . . . . . . . . . . .
+        . . + c . . . . . . . . . . . . . . . . . . . . . . .
+        . . . n . . . . . . . . . . . . . . . . . . . . . . .
+        . . . + c . . . . . . . . . . . . . . . . . c . . . .
+        . . . . n . . . . . . . . . . . . . . . . . c . . . .
+        . . . . + c . . . . . . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . . . . . . . . + c . . .
+        . . . . . n . . . . . . . . . . . . . . . . . c . . .
+        . . . . . + c . . . . . . . c . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . . . . . . . . . + c . .
+        . . . . . . n . . . . . . . c . . . . . . . . . n . .
+        . . . . . . + c . . . . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . + c . . . . . . . . . . .
+        . . . . . . . . . . . . . . . . . . . . . . . . + c .
+        . . . . . . . n . . . . . . . c . . . . . . . . . n .
+        . . . . . . . + c . . c . . . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . + c . . c . . . . . . .
+        . . . . . . . . . . . . . . . . . . . . . . . . . + c
+        . . . . . . . . n . . c . . . . n . . c . . . . . . n
+        . . . . . . . . + c c . . . . . . . . . . . . . . . .
+        . . . . . . . . . . . + c c . . . . . . . . . . . . .
+        . . . . . . . . . . . . . . . . + c c . . . . . . . .
+        . . . . . . . . . . . . . . . . . . . + c c . . . . .
+        . . . . . . . . . n c . n c . . . n c . n c . . . . .
+        + . . . . . . . . . + . . . . . . . + . . . . . . . +
+        . . . . . . . . . + . . . + . . . + . . . + . . . . .
+        . . . . . . . . . . . . + . . . . . . . + . . . . . .
       TXT
     end
   end
